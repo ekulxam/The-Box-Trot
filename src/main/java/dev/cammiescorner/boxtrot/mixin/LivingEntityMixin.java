7c863c55b84b9fe1293e19130053afc47cf1908c
CammiePone <inazuma261@gmail.com>
2022-04-15 15:10:56 -0400

    weeeeeee

--- src/main/java/dev/cammiescorner/boxtrot/mixin/LivingEntityMixin.java
+++ src/main/java/dev/cammiescorner/boxtrot/mixin/LivingEntityMixin.java
@@ -5,6 +5,7 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.particle.ParticleEffect;
@@ -12,6 +13,8 @@ import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin extends Entity {
@@ -24,5 +27,17 @@ public abstract class LivingEntityMixin extends Entity {
 	))
 	private boolean boxtrot$noParticles(World world, ParticleEffect parameters, double x, double y, double z, double velocityX, double velocityY, double velocityZ) {
 		return !(isSneaking() && getEquippedStack(EquipmentSlot.HEAD).isOf(Items.BARREL));
+	}
+
+	@Inject(method = "canTarget(Lnet/minecraft/entity/LivingEntity;)Z", at = @At("HEAD"), cancellable = true)
+	private void boxtrot$noTarget(LivingEntity target, CallbackInfoReturnable<Boolean> info) {
+		if(target instanceof PlayerEntity player && player.isSneaking() && player.getEquippedStack(EquipmentSlot.HEAD).isOf(Items.BARREL))
+			info.setReturnValue(false);
+	}
+
+	@Inject(method = "canSee", at = @At("HEAD"), cancellable = true)
+	private void boxtrot$noSee(Entity entity, CallbackInfoReturnable<Boolean> info) {
+		if(entity instanceof PlayerEntity player && player.isSneaking() && player.getEquippedStack(EquipmentSlot.HEAD).isOf(Items.BARREL))
+			info.setReturnValue(false);
 	}
 }
